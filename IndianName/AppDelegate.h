7b1f80e95641c9df773cd5f4249f155c512c4c63
상찬 이 <sangchan@sangchan-iui-Mac-mini.local>
2012-01-11 15:14:19 +0900

    Initial Commit

---
+++ IndianName/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IndianName
+//
+//  Created by 이 상찬 on 12. 1. 11..
+//  Copyright (c) 2012 KTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IndianName/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IndianName
+//
+//  Created by 이 상찬 on 12. 1. 11..
+//  Copyright (c) 2012 KTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
